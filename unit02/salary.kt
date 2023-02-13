1953d23ea80d52299145e2a6a0fd4b1b7e29e52e
Darcy Gaona <118694997+Dgaona01@users.noreply.github.com>
2023-02-12 23:51:52 -0600

    Create salary.kt

---
+++ unit02/salary.kt
@@ -0,0 +1,8 @@
+fun main(){
+    
+    var name = "Darcy Gaona"
+    var age = 23 
+    var annualPay= 10000.0
+    System.out.print("My name is "+name+", my age is "+age+" and I hope to earn $ "+annualPay+ " per year. ")
+      
+  }
