44e81e62411fe36b4f1a3f0c1ba0592f765019ce
Darcy Gaona <118694997+Dgaona01@users.noreply.github.com>
2023-04-18 09:43:27 -0500

    Create marketing.kt

---
+++ unit02/marketing.kt
@@ -0,0 +1,18 @@
+// Darcy Gaona 
+// April 18, 2023
+
+fun main() {
+    
+  
+ var friendsCount = 500 
+    
+if(friendsCount >  500)
+    {
+        println("Adopt a dog today!")
+    }
+else 
+    {
+        
+    println("Buy Cat Food at 20% Off")
+  }
+}
