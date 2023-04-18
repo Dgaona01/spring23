e60079c0486fe45dd41da3097eb5db2a7b87490c
Darcy Gaona <118694997+Dgaona01@users.noreply.github.com>
2023-04-18 11:30:31 -0500

    Delete salary.kt

--- unit02/salary.kt
+++
@@ -1,8 +0,0 @@
-fun main(){
-    
-    var name = "Darcy Gaona"
-    var age = 23 
-    var annualPay= 10000.0
-    System.out.print("My name is "+name+", my age is "+age+" and I hope to earn $ "+annualPay+ " per year. ")
-      
-  }
